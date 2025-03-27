7a9df250f40bca23153736dc530b28bc78c8adfc
s26582 <s26582@pjwstk.edu.pl>
2025-03-27 22:38:29 +0100

    Add new RIDER solution

---
+++ cw2/Interface.cs
@@ -0,0 +1,11 @@
+﻿namespace cw2;
+
+public interface IHazardNotifier
+{
+    void NotifyHazard(string message);
+}
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) { }
+}
